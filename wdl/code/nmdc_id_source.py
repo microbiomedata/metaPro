2b36edf43d5693c8b1cd9e1435d631522560a22c
Giberson, Cameron <cameron.giberson@pnnl.gov>
2024-02-15 15:15:31 -0800

    update ID sourcing class to include static methods using a token. add fake ID generator

--- wdl/code/nmdc_id_source.py
+++ wdl/code/nmdc_id_source.py
@@ -1,6 +1,10 @@
 import oauthlib
 import requests_oauthlib
 import functools
+import requests
+import json
+import random as r
+import string as s
 
 from datetime import datetime
 from typing import List
@@ -8,9 +12,11 @@ from json import dumps
 
 
 class NmdcIdSource():
-    def __init__(self, client_id=None, client_secret=None, buffer_time_mins=5):
+    def __init__(self, client_id=None, client_secret=None, username = None, password = None, buffer_time_mins=5):
         self._client_id = client_id
         self._client_secret = client_secret
+        self._username = username
+        self._password = password
         self._nmdc_mint_url = "https://api.microbiomedata.org/pids/mint"
         self._nmdc_token_url = "https://api.microbiomedata.org/token"
         self._buffer_time_mins = buffer_time_mins
@@ -36,8 +42,8 @@ class NmdcIdSource():
         def check_and_refresh(self, *args, **kwargs):
             if self._init_timestamp == None or ((datetime.now() - self._init_timestamp).total_seconds() / 60 > (self._expires_in_mins - self._buffer_time_mins)):
                 self._token = self._oauth.fetch_token(token_url=self._nmdc_token_url,
-                                                    client_id=self._client_id,
-                                                    client_secret=self._client_secret)
+                                                      client_id=self._client_id,
+                                                      client_secret=self._client_secret)
                 self._init_timestamp = datetime.now()
                 self._expires_in_mins = self._token['expires']['minutes']
             return method(self, *args, **kwargs)
@@ -58,3 +64,77 @@ class NmdcIdSource():
             raise Exception(f"Error getting ids from NMDC: {response.status_code} {response.text}")
 
         return response.json()
+    
+    @staticmethod
+    def get_token_from_password(username: str, password: str) -> str:
+        nmdc_token_url = 'https://api.microbiomedata.org/token'
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {
+            'grant_type': 'password',
+            'username': username,
+            'password': password,
+            'scope': '',
+            'client_id': '',
+            'client_secret': ''
+        }
+
+        response = requests.post(nmdc_token_url, data=data, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error getting token from NMDC: {response.status_code} {response.text}")
+            return None
+
+    @staticmethod
+    def get_ids_from_token(type: str, how_many: int, token: str):
+            url = "https://api.microbiomedata.org/pids/mint"
+            headers = {
+                'accept': 'application/json',
+                'Authorization': f'Bearer {token}',
+                'Content-Type': 'application/json'
+            }
+            payload = {
+                'schema_class': {
+                    'id': type,
+                },
+                'how_many': how_many
+            }
+            response = requests.post(url, data=payload, headers=headers)
+
+            if(response.status_code != 200):
+                raise Exception(f"Error getting ids from NMDC: {response.status_code} {response.text}")
+
+            return response.json()
+
+
+class NmdcFakeIdSource:
+    """
+        A Class for generating fake IDs for testing
+    """
+    def __init__(self, client_id=None, client_secret=None, username = None, password = None):        
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._username = username
+        self._password = password
+  
+    def get_ids(self, type: str, how_many: int) -> List[str]:
+        type_code: str = ""
+        shoulder = "1"
+
+        if type == "nmdc:DataObject":
+            type_code = "dobj"
+        elif type == "nmdc:MetaproteomicsAnalysisActivity":
+            type_code = "wfmp"
+        else:
+            type_code = "unkn"
+
+        ids = [
+            f"nmdc:{type_code}-{shoulder}-xxx" + ''.join(r.choices(s.ascii_lowercase + s.digits, k=5))
+            for _ 
+            in range(how_many)
+        ]
+
+        return ids
