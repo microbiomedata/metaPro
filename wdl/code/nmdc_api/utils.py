7b27b271a687e689fe6d5c4a50b1c32eac68701a
Giberson, Cam <cameron.giberson@gmail.com>
2026-09-23 01:14:13 -0700

    add zero-dependency version of file download. change interface for both

--- wdl/code/nmdc_api/utils.py
+++ wdl/code/nmdc_api/utils.py
@@ -4,9 +4,10 @@ from pathlib import Path
 from typing import Optional
 
 
-def download_file(url, base_path: Path) -> Optional[str]:
+def download_file(url, base_path: str) -> Optional[str]:
+    to_return = None
     file_name = os.path.basename(url)
-    filepath = base_path / file_name
+    filepath = Path(base_path) / file_name
 
     print(f"File downloading {file_name} from {url}")
 
@@ -16,7 +17,9 @@ def download_file(url, base_path: Path) -> Optional[str]:
         with open(filepath, 'wb') as fp:
             fp.write(response.content)
         print(f"File downloaded and saved as {filepath}")
-        return str(filepath.absolute())
+        to_return = str(filepath.absolute())
     else:
         print(f"Failed to download file. Status code: {response.status_code}")
-        return None
+        to_return = None
+
+    return to_return
---
+++ wdl/code/nmdc_api/utils_no_deps.py
@@ -0,0 +1,25 @@
+import urllib.request
+import shutil
+import os
+from pathlib import Path
+from typing import Optional
+
+
+def download_file_1(url: str, base_path: str) -> Optional[str]:
+    to_return = None
+    file_name = os.path.basename(url)
+    filepath = Path(base_path) / file_name
+
+    print(f"File downloading {file_name} from {url}")
+
+    with urllib.request.urlopen(url) as response:
+        if response.status == 200:
+            with open(filepath, 'wb') as fp:
+                shutil.copyfileobj(response, fp)
+            print(f"File downloaded and saved as {filepath}")
+            to_return = str(filepath.absolute())
+        else:
+            print(f"Failed to download file. Status code: {response.status}")
+            to_return = None
+
+    return to_return
